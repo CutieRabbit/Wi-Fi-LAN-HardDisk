94fdbde1f22ed62f2b19cac43f61b562fac31212
CutieRabbit <sigtunatw@gmail.com>
2020-03-01 22:16:30 +0800

    將IP顯示分成Wifi-IP跟Device-IP，在伺服器運行時能夠給予一個Ongoing運行通知。

--- app/src/main/java/com/example/mobileftpserver/MainActivity.java
+++ app/src/main/java/com/example/mobileftpserver/MainActivity.java
@@ -1,36 +1,41 @@
 package com.example.mobileftpserver;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.DialogInterface;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.FileUtils;
-import android.text.format.Formatter;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.FtpServerFactory;
-import org.apache.ftpserver.ftplet.FtpException;
 import org.apache.ftpserver.listener.ListenerFactory;
 import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
-import org.apache.ftpserver.usermanager.impl.BaseUser;
-import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     FtpServerFactory serverFactory;
+    FtpServer server;
     ListenerFactory listenerFactory;
 
     private static String[] PERMISSIONS_STORAGE = {
@@ -48,13 +53,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void openServer(View view) {
 
         EditText editText = (EditText) findViewById(R.id.editText2);
         String stringPort = editText.getText().toString();
 
         int port = Integer.parseInt(stringPort);
-        String ipAddress = getIP();
+        String deviceIP = getDeviceIP();
+        String wifiIP = getWifiIP();
+
+        if(wifiIP.equals("0.0.0.0")){
+            wifiIP = "不可用";
+        }
 
         if(port >= 65535 || port <= 0) {
             wrongPort();
@@ -66,21 +77,34 @@ public class MainActivity extends AppCompatActivity {
         factory.setPort(port);
         setConfigurationFile();
         serverFactory.addListener("default", factory.createListener());
-        FtpServer server = serverFactory.createServer();
+        server = serverFactory.createServer();
 
         try {
             server.start();
         }catch(Exception e){
             e.printStackTrace();
             openServerFailed(e.getMessage());
+            return;
         }
 
         TextView serverPortView = (TextView) findViewById(R.id.ServerPort);
         TextView serverStatus = (TextView) findViewById(R.id.ServerStatus);
-        TextView serverIP = (TextView) findViewById(R.id.serverIP);
+        TextView deviceIPView = (TextView) findViewById(R.id.DeviceIP);
+        TextView wifiIPView = (TextView) findViewById(R.id.WiFi_IP);
         serverPortView.setText(String.format("伺服器Port: %d", port));
         serverStatus.setText(String.format("伺服器狀態: %s", "開啟"));
-        serverIP.setText(String.format("伺服器IP: %s:%d",ipAddress, port));
+        deviceIPView.setText(String.format("設備IP: %s:%d",deviceIP, port));
+
+        if(!wifiIP.equals("不可用")) {
+            wifiIPView.setText(String.format("Wi-Fi IP: %s:%d", wifiIP, port));
+        }else{
+            wifiIPView.setText(String.format("Wi-Fi IP: %s", wifiIP));
+        }
+
+        Button startServerButton = (Button) findViewById(R.id.button);
+        startServerButton.setEnabled(false);
+
+        sendNotification(port);
 
     }
 
@@ -112,11 +136,28 @@ public class MainActivity extends AppCompatActivity {
         alertBox.show();
     }
 
-    public String getIP(){
+    public String getWifiIP(){
         WifiManager wifiManager = (WifiManager) getApplication().getApplicationContext().getSystemService(WIFI_SERVICE);
         int intIP = wifiManager.getConnectionInfo().getIpAddress();
         String ipAddress = String.format(Locale.getDefault(), "%d.%d.%d.%d", (intIP & 0xff), (intIP >> 8 & 0xff), (intIP >> 16 & 0xff), (intIP >> 24 & 0xff));
         return ipAddress;
+    }
+
+    public String getDeviceIP(){
+        try{
+            List<NetworkInterface> list = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for(NetworkInterface intf : list){
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for(InetAddress address : addrs){
+                    if(!address.isLoopbackAddress() && !address.getHostAddress().contains(":")){
+                        return address.getHostAddress();
+                    }
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return "Unknown Error";
     }
 
     public void setConfigurationFile(){
@@ -152,4 +193,24 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+    public void closeServer(){
+        server.stop();
+    }
+
+    public void sendNotification(int port){
+        NotificationUtils utils = new NotificationUtils();
+        String title = "伺服器運作中";
+        String text = "運行Port: " + port;
+        String id = utils.createNotificationChannel(this);
+        NotificationCompat.Builder compat = new NotificationCompat.Builder(this, id);
+        compat.setContentTitle(title)
+                .setContentText(text)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationManager.IMPORTANCE_HIGH)
+                .setOngoing(true);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        notificationManagerCompat.notify(0, compat.build());
+    }
+
 }
